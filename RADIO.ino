a543451eab304e9dd20a925dd2195aa911c9caf0
seemywings <kevin.jayne@icloud.com>
2024-04-24 18:32:19 -0400

    working on esp8266

--- RADIO.ino
+++ RADIO.ino
@@ -1,59 +1,30 @@
 
 #include <ESPWiFi.h>
 
-#include "Audio.h"
-#include "OpenAI.h"
-#include "Utils.h"
-
-// WeMos Pin Config:
-// RX Radio SPK: A0
-// TX Radio PTT: D6 (12)
-// TX Audio DIN: RX (03)
-// TX Audio BCK: D8 (15)
-// TX Audio LCK: D4 (02)
-
 // Web Server
-const String webServerName = "RADIO";
-ESPWiFi wifi = ESPWiFi("RADIO", "abcd1234");
+ESPWiFi wifi;
+
+bool runAtStart = false;
+String ttsFilePath = "/tts.mp3";
 
 void setup() {
-  initializeData();
-  initializeWebServer();
-  initializeAudio();
+  wifi.start();
+  wifi.startAudio();
+  wifi.pttEnabled = true;
 }
-
-boolean runOnBoot = false;
-
-unsigned long lastLoopRun = 0;
 
 void loop() {
   wifi.handleClient();
-  handleAudio([]() {
-    Serial.println("Responding to RX");
-    String whatToSay = openAIChat(
-        "Provide a question and answer for the HAM radio technician exam.");
-    Serial.println(whatToSay);
-    String ttsFile = "/tts.mp3";
-    openAI_TTS(whatToSay, ttsFile);
-    playAudioFile(ttsFile);
+  wifi.handleAudio([]() {
+    Serial.println("Responding to audio transmission");
+    // String response = wifi.openAIChat("HAM Radio Facts and Trivia!");
+    // wifi.openAI_TTS(response, ttsFilePath);
+    // wifi.playMP3(ttsFilePath);
   });
-
-  if (!runOnBoot) {
-    runOnBoot = true;
-    String ttsFile = "/tts.mp3";
-    String hello = openAIChat("Say hello and introduce yourself.");
-    Serial.println(hello);
-    openAI_TTS(hello, ttsFile);
-    playAudioFile(ttsFile);
+  if (!runAtStart) {
+    runAtStart = true;
+    String response = wifi.openAIChat("Hello, world!");
+    wifi.openAI_TTS(response, ttsFilePath);
+    wifi.playMP3(ttsFilePath);
   }
-}
-
-void initializeData() {
-  Serial.println("R.A.D.I.O. starting up...");
-  readOpenAIKey("/openAI.key");
-}
-
-void initializeWebServer() {
-  wifi.enableMDNS(webServerName);
-  wifi.start();
 }

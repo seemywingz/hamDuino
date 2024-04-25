7ca7931a54791aae2092d893d1dea3dff01978ed
seemywings <kevin.jayne@icloud.com>
2024-04-25 15:53:49 -0400

    testing bell mp3

--- RADIO.ino
+++ RADIO.ino
@@ -8,13 +8,14 @@ String ttsFilePath = "/tts.mp3";
 
 void setup() {
   wifi.start();
-  wifi.startAudio();
+  wifi.startAudio(2.7);
   wifi.pttEnabled = true;
   wifi.audioResponse = [](){
     Serial.println("Responding to Radio Transmission");
-    String response = wifi.openAI_Chat("Provide a question and answer from the technician's HAM radio exam.");
-    wifi.openAI_TTS(response, ttsFilePath);
-    wifi.playMP3(ttsFilePath);
+    wifi.playMP3("/bell.mp3"); 
+    // String response = wifi.openAI_Chat("Provide a question and answer from the technician's HAM radio exam.");
+    // wifi.openAI_TTS(response, ttsFilePath);
+    // wifi.playMP3(ttsFilePath);
   };
   String response = wifi.openAI_Chat("Hello, world!");
   wifi.openAI_TTS(response, ttsFilePath);

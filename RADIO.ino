201e4fd8c86af990e5e3f669309e2a0f57263f33
seemywings <kevin.jayne@icloud.com>
2024-04-25 00:50:59 -0400

    minor fixes

--- RADIO.ino
+++ RADIO.ino
@@ -4,27 +4,25 @@
 // Web Server
 ESPWiFi wifi;
 
-bool runAtStart = false;
 String ttsFilePath = "/tts.mp3";
 
 void setup() {
   wifi.start();
   wifi.startAudio();
   wifi.pttEnabled = true;
+  wifi.audioResponse = [](){
+    Serial.println("Responding to Radio Transmission");
+    String response = wifi.openAI_Chat("Provide a question and answer from the technician's HAM radio exam.");
+    wifi.openAI_TTS(response, ttsFilePath);
+    wifi.playMP3(ttsFilePath);
+  };
+  String response = wifi.openAI_Chat("Hello, world!");
+  wifi.openAI_TTS(response, ttsFilePath);
+  wifi.playMP3(ttsFilePath);
 }
 
 void loop() {
   wifi.handleClient();
-  wifi.handleAudio([]() {
-    Serial.println("Responding to audio transmission");
-    // String response = wifi.openAIChat("HAM Radio Facts and Trivia!");
-    // wifi.openAI_TTS(response, ttsFilePath);
-    // wifi.playMP3(ttsFilePath);
-  });
-  if (!runAtStart) {
-    runAtStart = true;
-    String response = wifi.openAIChat("Hello, world!");
-    wifi.openAI_TTS(response, ttsFilePath);
-    wifi.playMP3(ttsFilePath);
-  }
+  wifi.handleAudio();
 }
+
